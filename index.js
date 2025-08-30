a2d470e230fd5eb2638d7cbb4f608692cd99ccca
rhamza783-lang <hamzayounis783@gmail.com>
2025-08-30 13:54:07 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * index.js - Entry point for React Native
+ */
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent() with the tag name 'main'
+registerRootComponent(App);
